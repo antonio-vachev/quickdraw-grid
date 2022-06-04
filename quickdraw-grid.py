7a6f8a050fed4c5968844610683049287f5e6cfc
Antonio <vachevantonio@gmail.com>
2022-06-04 19:14:44 +0300

    Add categories module, generate random categories

--- quickdraw-grid.py
+++ quickdraw-grid.py
@@ -2,19 +2,34 @@ from PIL import Image, ImageDraw
 from quickdraw.data import QuickDrawData
 import svgwrite
 import os
+import random
+from categories import categories_list
 
 # Collect user's choice of category (repeat until valid category is selected)
+print('What is your "Quick,Draw!" category of choice?')
 while True:
     qd = QuickDrawData()
-    selected_category = str.lower((input("What is your category of choice? ")))
+
+    # Generate 3 random Quick,draw categories as examples for the user
+    random_categories = []
+    for element in range(3):
+        random_selection = random.choice(categories_list)
+        while random_selection in random_categories:
+            random_selection = random.choice(categories_list)
+        random_categories.append(random_selection)
+
+    # Prompt a reply from the user
+    selected_category = str.lower((input(f"Some random categories are:"
+                                         f" {random_categories[0]}, {random_categories[1]}, "
+                                         f"{random_categories[2]}.\nYour choice is: ")))
     try:
         selected_item = qd.get_drawing(selected_category)
         break
     except ValueError:
-        print(f"{selected_category} is not a valid category. Try another one.")
+        print(f"\n{selected_category} is not a valid category. Try another one.")
 
 # Collect user's choice of number of columns and rows
-rows = int(input("How many rows do you want? "))
+rows = int(input("\nHow many rows do you want? "))
 columns = int(input("How many columns do you want? "))
 rows_columns = rows * columns
 rows_count = 0
@@ -31,8 +46,8 @@ dwg = svgwrite.Drawing(f'{selected_category}{file_index}.svg', profile='tiny')
 
 # Get the specified number of drawing from QuickDraw's database
 for image in range(rows_columns):
-    row_offset = rows_count * 500
-    column_offset = columns_count * 500
+    row_offset = rows_count * 400
+    column_offset = columns_count * 400
     qd = QuickDrawData()
     selected_item = qd.get_drawing(selected_category)
     item_image = Image.new("RGB", (255, 255), color=(255, 255, 255))
@@ -47,8 +62,8 @@ for image in range(rows_columns):
             y2 = stroke[coordinate + 1][1]
             item_drawing.line((x1, y1, x2, y2), fill=(0, 0, 0), width=2)
             dwg.add(dwg.line((x1 + column_offset, y1 + row_offset),
-                    (x2 + column_offset, y2 + row_offset),
-                        stroke=svgwrite.rgb(10, 10, 16, '%')))
+                             (x2 + column_offset, y2 + row_offset),
+                             stroke=svgwrite.rgb(10, 10, 16, '%')))
 
     # Increase rows / columns count
     if columns_count < columns - 1:
